f6af2e5e694f6e1d0d0a55cbf0c98a44c9d5817b
山本直樹 <yamamotonaoki@ny-mbp.local>
2019-07-31 17:12:14 +0900

    [Create] 投稿・編集・更新・削除機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  # get 'books/show'
+  # get 'books/index'
+  # get 'books/new'
+  # get 'books/edit'
+
+  resources :books
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
